0a5abe4947f41ceb11e1a615082b1d80994f37cb
erlanggaard <erlanggaard131@gmail.com>
2021-04-04 00:51:22 +0700

    Routing Admin ke User

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\PelangganController;
 
+//Route Admin
 Route::get('/', 'App\Http\Controllers\AdminController@index');
 
+//end Route Admin
+
+
+//Route Supplier
 Route::get('supplier', [SupplierController::class, 'index'])->name('supplier.supplier');
 Route::get('create-supplier', [SupplierController::class, 'create'])->name('supplier.create-supplier');
 Route::post('save-supplier', [SupplierController::class, 'store'])->name('supplier.simpan-supplier');
+//end Route Supplier
 
+//Route Pelanggan
 Route::get('pelanggan', [PelangganController::class, 'index'])->name('pelanggan.pelanggan');
 Route::get('create-pelanggan', [PelangganController::class, 'create'])->name('pelanggan.create-pelanggan');
 Route::post('save-pelanggan', [PelangganController::class, 'store'])->name('pelanggan.simpan-pelanggan');
+//end Route Pelanggan
