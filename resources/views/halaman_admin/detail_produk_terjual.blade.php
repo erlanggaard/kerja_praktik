3139d3c55569baf8fd52a3621a10bf83c3482cd5
erlanggaard <erlanggaard131@gmail.com>
2021-04-12 22:51:37 +0700

    card

--- resources/views/halaman_admin/detail_produk_terjual.blade.php
+++ resources/views/halaman_admin/detail_produk_terjual.blade.php
@@ -1,1 +1,21 @@
 @extends('layout/main')
+
+@section('container')
+
+<div class="row">
+    <div class="col-12 col-sm-6 col-lg-6">
+        <div class="card mt-5">
+            <div class="card-header">
+                <h4>Detail Transaksi</h4>
+            </div>
+            <div class="card-body">
+
+            </div>
+            <div class="card-footer">
+                Card Footer
+            </div>
+
+        </div>
+    </div>
+
+    @endsection
